1dc33ee2a54214a0460a00933fdcfea448f09bca
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-31 11:52:46 -0700

    Reading HTML data part 2

--- src/com/bibleit/mvc/HelloWorldController.java
+++ src/com/bibleit/mvc/HelloWorldController.java
@@ -1,5 +1,17 @@
 package com.bibleit.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloWorldController {
 
+	// need a controller method to show the initial form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// need a controller methodto process the HTML form
+	
 }
