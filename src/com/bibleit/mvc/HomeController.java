a9a36c2ba8ae5aed8e61d67a34296e58c1d169b8
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-31 11:20:11 -0700

    Creating a Spring Home Controller and View Part 2

--- src/com/bibleit/mvc/HomeController.java
+++ src/com/bibleit/mvc/HomeController.java
@@ -1,5 +1,14 @@
 package com.bibleit.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
 
 }
